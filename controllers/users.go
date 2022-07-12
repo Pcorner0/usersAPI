f6baf43df1abe69ed563021faa4b72c9e16fb731
Pcorner0 <princon04.20@gmail.com>
2022-07-12 17:53:43 -0500

    Works with DB postgres

--- controllers/users.go
+++ controllers/users.go
@@ -23,21 +23,35 @@ func NewRepoUsers() *RepoUsers {
 	return &RepoUsers{DB: db}
 }
 
+// CreateUser method CreateUser using the echo.Context
 func (u *RepoUsers) CreateUser(c echo.Context) error {
-	User := models.Users{}
-
-	if err := c.Bind(&User); err != nil {
-		c.JSON(http.StatusBadRequest, err)
+	User := &models.Users{}
+	if err := c.Bind(User); err != nil {
 		return err
 	}
-
-	err := models.CreateUser(u.DB, &User)
+	err := models.CreateUser(u.DB, User)
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, err)
+		return err
 	}
 	return c.JSON(http.StatusOK, User)
 }
 
+/*func (u *RepoUsers) CreateUser(c echo.Context) error {
+	User := &models.Users{}
+
+	if err := c.Bind(User); err != nil {
+		c.JSON(http.StatusBadRequest, err)
+		return err
+	}
+
+	err := models.CreateUser(u.DB, User)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+	return c.JSON(http.StatusOK, User)
+}*/
+
+// GetUser shows a user by id
 func (u *RepoUsers) GetUser(c echo.Context) error {
 	id := c.Param("id")
 	User := &models.Users{}
